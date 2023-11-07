07441a104e863ad1a1f0b473261ad1dfad5b0e2e
Paulo Cotta <cotta.paulo@gmail.com>
2023-11-07 10:50:53 -0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -20,3 +20,32 @@ COLLATE='utf8mb4_0900_ai_ci'
 ENGINE=InnoDB
 AUTO_INCREMENT=12
 ;
+
+
+
+ALTER ALGORITHM = UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `GetCurls` AS select trim(concat(`curls`.`vhash`,' ',ifnull(`curls`.`token`,'_'),' ',replace(to_base64(`curls`.`cmd`),'\n',''),' ',lpad(if((`curls`.`now_status_code` <> `curls`.`status_code`),`curls`.`now_status_code`,`curls`.`status_code`),3,'0'),' ',if((`curls`.`now_tps` > `curls`.`tps`),-(1),`curls`.`tps`),' ')) AS `data` from `curls` where ((`curls`.`ativo` = 'S') and ('comment' <> 'Se now_status_code <> status_code, considera now_status_code para evitar atualização no banco (write), se não status_code') and ('comment' <> 'Se now_tps > tps, considera -1 para forçar a atualização, se não tps')) order by rand() ;
+ALTER ALGORITHM = UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `GetCurlsJson` AS select json_arrayagg(json_object('id',`curls`.`id`,'vhash',`curls`.`vhash`,'descricao',`curls`.`descricao`,'now_status_code',`curls`.`now_status_code`,'now_tps',`curls`.`now_tps`,'status',`curls`.`status`)) AS `jdata` from `curls` where (`curls`.`ativo` = 'S') ;
+
+CREATE DEFINER=`root`@`localhost` TRIGGER `curls_before_update` BEFORE UPDATE ON `curls` FOR EACH ROW BEGIN
+	SET
+		NEW.`cmd`             = NULLIF(TRIM(REGEXP_REPLACE(NEW.`cmd`,'[[:space:]][[:space:]]+|\\\\$|\n', '', 1, 0, 'imu')),''),
+		NEW.`vhash`           = NULLIF(TRIM(SHA1(NEW.`cmd`)),''),
+		NEW.`descricao`       = NULLIF(TRIM(NEW.`descricao`),''),
+		NEW.`token`           = NULLIF(TRIM(NEW.`token`),''),
+		NEW.`status_code`     = NULLIF(TRIM(NEW.`status_code`),''),
+		NEW.`tps`             = NULLIF(TRIM(NEW.`tps`),''),
+		NEW.`now_status_code` = NULLIF(TRIM(NEW.`now_status_code`),''),
+		NEW.`now_tps`         = NULLIF(TRIM(NEW.`now_tps`),''),
+		NEW.`ativo`           = NULLIF(TRIM(UPPER(NEW.`ativo`)),'');
+
+	#:Controla o status (farol)
+	IF ( NEW.`status_code` != NEW.`now_status_code` ) THEN
+		SET NEW.`status` = 3;
+	ELSE
+		IF ( NEW.`now_tps` > NEW.`tps` ) THEN
+			SET NEW.`status` = 2;
+		ELSE
+			SET NEW.`status` = 1;
+		END IF;
+	END IF;
+END
